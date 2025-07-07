c6dffac106c71faf6e598946a6274fd7004fba54
risherlock <risherlock221b@gmail.com>
2025-07-07 18:36:32 +0200

    minor cleanup of `get_lambda_max()`
    I am using ESOQ-2, on the `adcs` repo, to compute the average of quaternions. In particular, it is used to perform eigenanalysis on the matrix K. The code works, but there is a discrepancy in the computation of the optimal quaternion that I have yet to investigate.
    
    Now that I think about it, I believe I should implement an additional function to compute max eigenvector corresponding to `lambda_max`. This way, the same code can be used in both codebases.

--- c/esoq2.c
+++ c/esoq2.c
@@ -9,22 +9,16 @@ static inline double det_3x3(double m[3][3])
          m[0][2] * (m[1][0] * m[2][1] - m[1][1] * m[2][0]);
 }
 
-// Compute the greatest eigenvalue of symmetric 4x4 matrix
+// Compute greatest eigenvalue of traceless symmetric 4x4 matrix
 double get_lambda_max(const double K[4][4], const int n)
 {
-  // Cache unique elements of K
-  const double K00 = K[0][0]; // S00
-  const double K01 = K[0][1]; // S01
-  const double K02 = K[0][2]; // S02
-  const double K03 = K[0][3]; // z0
-  const double K11 = K[1][1]; // S11
-  const double K12 = K[1][2]; // S12
-  const double K13 = K[1][3]; // z1
-  const double K22 = K[2][2]; // S22
-  const double K23 = K[2][3]; // z2
-  const double K33 = K[3][3]; // trB
+  // Cache unique elements of the symmetric matrix
+  const double K00 = K[0][0], K01 = K[0][1], K02 = K[0][2], K03 = K[0][3];
+  const double K11 = K[1][1], K12 = K[1][2], K13 = K[1][3];
+  const double K22 = K[2][2], K23 = K[2][3];
+  const double K33 = K[3][3];
 
-  // Cache frequently used terms
+  // Cache frequently used square terms
   const double K03_sq = K03 * K03;
   const double K13_sq = K13 * K13;
   const double K23_sq = K23 * K23;
@@ -32,7 +26,7 @@ double get_lambda_max(const double K[4][4], const int n)
   const double K01_sq = K01 * K01;
   const double K02_sq = K02 * K02;
 
-  // Eqn. 7
+  // Trace and adjugate terms. Eqn.(7)
   const double trB = K33;
   const double tradB = // trace(adj(B + tr(B)))
       (K11 + trB) * (K22 + trB) - K12_sq +
@@ -49,34 +43,34 @@ double get_lambda_max(const double K[4][4], const int n)
       2.0 * K01 * K02 * K23 * K13 + K11 * K00 * K33 * K22 - K00 * K13_sq * K22 -
       K01_sq * K33 * K22 + 2.0 * K01 * K03 * K13 * K22 - K11 * K03_sq * K22;
 
-  double lambda_max = 0.0;
-
-  // Compute maximum eigenvalue based on number of vector pairs
+  // Two quaternions
   if (n == 2)
   {
-    const double g3 = sqrt(2 * sqrt(d) - b);  // Eqn. 15
-    const double g4 = sqrt(-2 * sqrt(d) - b); // Eqn. 15
-    lambda_max = (g3 + g4) / 2.0f;            // Eqn. 14
-  }
-  else
-  {
-    // trace(adj(K))
-    const double tradK =
-        det_3x3((double[3][3]){{K11, K12, K13}, {K12, K22, K23}, {K13, K23, K33}}) +
-        det_3x3((double[3][3]){{K00, K02, K03}, {K02, K22, K23}, {K03, K23, K33}}) +
-        det_3x3((double[3][3]){{K00, K01, K03}, {K01, K11, K13}, {K03, K13, K33}}) +
-        det_3x3((double[3][3]){{K00, K01, K02}, {K01, K11, K12}, {K02, K12, K22}});
-
-    const double c = -tradK;                                                       //  Eqn. 7
-    const double p = pow(b / 3.0f, 2) + 4.0f * d / 3;                              // Eqn. 10
-    const double q = pow(b / 3.0f, 3) - 4.0f * d * b / 3.0f + c * c / 2.0;         // Eqn. 10
-    const double u1 = 2.0f * sqrt(p) * cos(acos(q / pow(p, 1.5)) / 3.0) + b / 3.0; // Eqn. 9
-    const double g1 = sqrt(u1 - b);                                                // Eqn. 12
-    const double g2 = -2.0f * sqrt(u1 * u1 - 4.0 * d);                             // Eqn. 12
-    lambda_max = 0.5f * (g1 + sqrt(-u1 - b - g2));                                 // Eqn. 13
+    const double sqrt_d = sqrt(d);
+    const double g3 = sqrt(2 * sqrt_d - b);  // Eqn.(15)
+    const double g4 = sqrt(-2 * sqrt_d - b); // Eqn.(15)
+    return (g3 + g4) / 2.0f;                 // Eqn.(14)
   }
 
-  return lambda_max;
+  // trace(adj(K))
+  double m1[3][3] = {{K11, K12, K13}, {K12, K22, K23}, {K13, K23, K33}};
+  double m2[3][3] = {{K00, K02, K03}, {K02, K22, K23}, {K03, K23, K33}};
+  double m3[3][3] = {{K00, K01, K03}, {K01, K11, K13}, {K03, K13, K33}};
+  double m4[3][3] = {{K00, K01, K02}, {K01, K11, K12}, {K02, K12, K22}};
+  const double tradK = det_3x3(m1) + det_3x3(m2) + det_3x3(m3) + det_3x3(m4);
+  const double c = -tradK; // Eqn.(7)
+
+  // Eqn.(10)
+  const double p = b * b / 9.0 + 4.0 * d / 3.0;
+  const double q = b * b * b / 27.0 - 4.0 * d * b / 3.0 + c * c / 2.0;
+
+  // Eigenvalue solution
+  const double arg = q / pow(p, 1.5);
+  const double theta = acos(fmax(-1.0, fmin(1.0, arg)));
+  const double u1 = 2.0 * sqrt(p) * cos(theta / 3.0) + b / 3.0; // Eqn.(9)
+  const double g1 = sqrt(u1 - b);                               // Eqn.(12)
+  const double g2 = -2.0 * sqrt(u1 * u1 - 4.0 * d);             // Eqn.(12)
+  return 0.5f * (g1 + sqrt(-u1 - b - g2));                      // Eqn.(13)
 }
 
 quaternion_t esoq2_update(const esoq2_vec_t *vb, const esoq2_vec_t *vi, double *w, const int n)
@@ -113,13 +107,13 @@ quaternion_t esoq2_update(const esoq2_vec_t *vb, const esoq2_vec_t *vi, double *
   const double K[4][4] = {{S00, S01, S02, z[0]}, {S01, S11, S12, z[1]}, {S02, S12, S22, z[2]}, {z[0], z[1], z[2], trB}};
   const double lambda = get_lambda_max(K, n);
 
-  // Eqn. 18
-  const double t = trB - lambda;
+  // Eqn.(18)
   S00 = S00 - lambda;
   S11 = S11 - lambda;
   S22 = S22 - lambda;
+  const double t = trB - lambda;
 
-  // Entries of matrix M in Eqn. 20 computed using Eqn. 19
+  // Entries of matrix M in Eqn.(20) computed using Eqn.(19)
   const double ma = S00 * t - z[0] * z[0];
   const double mb = S11 * t - z[1] * z[1];
   const double mc = S22 * t - z[2] * z[2];
@@ -130,7 +124,7 @@ quaternion_t esoq2_update(const esoq2_vec_t *vb, const esoq2_vec_t *vi, double *
   double e[3] = {mb * mc - mz * mz, ma * mc - my * my, ma * mb - mx * mx};
   double e_abs[3] = {fabs(e[0]), fabs(e[1]), fabs(e[2])};
 
-  // Choose optimal principal axis from three choices. Eqn. 21
+  // Choose optimal principal axis from three choices. Eqn.(21)
   if ((e_abs[0] > e_abs[1]) && (e_abs[0] > e_abs[2]))
   {
     e[1] = my * mz - mx * mc;
@@ -153,7 +147,7 @@ quaternion_t esoq2_update(const esoq2_vec_t *vb, const esoq2_vec_t *vi, double *
   e[1] *= norm_e_inv;
   e[2] *= norm_e_inv;
 
-  // Find maximum absolute value among z components and t
+  // Find maximum absolute value among the elements of z and t
   const double abs_z0 = fabs(z[0]);
   const double abs_z1 = fabs(z[1]);
   const double abs_z2 = fabs(z[2]);
@@ -183,7 +177,7 @@ quaternion_t esoq2_update(const esoq2_vec_t *vb, const esoq2_vec_t *vi, double *
     yk = z[0] * e[0] + z[1] * e[1] + z[2] * e[2];
   }
 
-  // Eqn. 28
+  // Eqn.(28)
   const double h = sqrt(xk * xk + yk * yk);
   const double h_inv = 1.0 / h;
   const double sph = xk * h_inv;
